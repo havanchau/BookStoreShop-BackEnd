9eab8226d648157f3836e00088f7fecd56557b25
havanchau <chauhvc2.dy@gmail.com>
2024-04-25 21:06:14 +0700

    fix: hide key for .env

--- config/db.config.js
+++ config/db.config.js
@@ -1,18 +1,20 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'havanchau2510',
-  database: 'assignmentdb'
+import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } from "../ultils/contranst";
+
+const connection = await mysql.createConnection({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to MySQL:', err);
+    console.error("Error connecting to MySQL:", err);
     return;
   }
-  console.log('Connected to MySQL');
+  console.log("Connected to MySQL");
 });
 
 module.exports = connection;
